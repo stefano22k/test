cd2a2dc7bab57999e5a116d9e4e2eed309e9c12f
stefano22k <stragastefano@gmail.com>
2024-07-02 12:06:28 -0300

    agregando la columna de registro

--- index.php
+++ index.php
@@ -1,13 +1,43 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Crud php</title>
 </head>
+
 <body>
-    
+    <h1 class="text-center p-3">Crud con PHP</h1>
+    <div class="container-fluid row">
+        <form class="col-4 p-3">
+            <h3 class="text-center text-secondary">Registro:</h3>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Nombre:</label>
+                <input type="text" class="form-control" name="nombre">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Apellido:</label>
+                <input type="text" class="form-control" name="apellido">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">DNI:</label>
+                <input type="text" class="form-control" name="dni">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Fecha de nacimiento:</label>
+                <input type="date" class="form-control" name="fecha">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Correo:</label>
+                <input type="email" class="form-control" name="correo">
+            </div>
+            <button type="submit" class="btn btn-primary" name="btn-registrar" value="ok">Registrar</button>
+        </form>
+    </div>
+    <!-- script bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
