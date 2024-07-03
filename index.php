e97307352f03f5c11010859f6ef6386c5da04b1e
stefano22k <stragastefano@gmail.com>
2024-07-03 16:28:07 -0300

    agregando fontawesome y botones de editar y eliminar

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/139fcb8934.js" crossorigin="anonymous"></script>
     <title>Crud php</title>
 </head>
 
@@ -59,8 +60,8 @@
                         <td>Otto</td>
                         <td>@mdo</td>
                         <td>
-                            <a href="">Editar</a>
-                            <a href="">Eliminar</a>
+                            <a href="" class="btn btn-small btn-warning"><i class="fa-solid fa-pen-to-square"></i></a>
+                            <a href="" class="btn btn-small btn-danger"><i class="fa-solid fa-trash"></i></a>
                         </td>
                     </tr>
                     
